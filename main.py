b9d2e1c43ab5ed545929879bcb7f41ba7624c8a7
Chia Hao Hsu Tai <hsutai.ch@northeastern.edu>
2025-05-07 21:47:54 -0400

    initial test

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+from unoserver.client import UnoClient
+
+uno_client = UnoClient()
+
+in_file = "sample.docx"
+out_file = "sample.pdf"
+
 def main():
-    print("Hello from sofficeapi!")
+    uno_client.convert(inpath=in_file, outpath=out_file, convert_to="pdf")
 
 if __name__ == "__main__":
     main()
