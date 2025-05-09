c1fcd7e4ea51e24604fb19c534516ae776efab97
Chia Hao Hsu Tai <hsutai.ch@northeastern.edu>
2025-05-08 23:16:54 -0400

    support for multiple soffice instances

--- src/conversions/service.py
+++ src/conversions/service.py
@@ -1,6 +1,23 @@
+from contextlib import contextmanager
+from typing import Generator
+
+from box import Box
 from unoserver.client import UnoClient
 
-uno_client = UnoClient()
+PORTS = [2000, 2001]
+workers = [Box({"client": UnoClient(port=p), "load": 0}) for p in PORTS]
+
+
+@contextmanager
+def uno_worker(size: int) -> Generator[UnoClient, None, None]:
+    worker = min(workers, key=lambda x: x.load)
+    worker.load += size
+    try:
+        yield worker.client
+    finally:
+        worker.load -= size
+
 
 def convert_file(file: bytes, convert_to: str) -> bytes:
-    return uno_client.convert(indata=file, convert_to=convert_to, outpath=None)
+    with uno_worker(len(file)) as uno_client:
+        return uno_client.convert(indata=file, convert_to=convert_to, outpath=None)
